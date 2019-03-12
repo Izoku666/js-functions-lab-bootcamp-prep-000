9fa3a9858cf880e93c9a45aab9a714d922a9cc4a
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-12 03:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays()
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name)
+function happyHolidaysTo(string name)
 {
-  return "Happy Holidays \$\{name\}!"
+  return "Happy Holidays \$\{${name}\}!"
 }
